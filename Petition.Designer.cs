921d5d44ed53400b5ce5f8e63296dfe0038bb7e3
fulfordcs <fulfordcs@gmail.com>
2023-03-29 11:51:19 +0100

    Add project files.

---
+++ Petition.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PetitionGrapher
+{
+    partial class Petition
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetData = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGetData
+            // 
+            this.btnGetData.Location = new System.Drawing.Point(581, 9);
+            this.btnGetData.Name = "btnGetData";
+            this.btnGetData.Size = new System.Drawing.Size(166, 24);
+            this.btnGetData.TabIndex = 0;
+            this.btnGetData.Text = "Analyse Data";
+            this.btnGetData.UseVisualStyleBackColor = true;
+            this.btnGetData.Click += new System.EventHandler(this.btnGetData_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL of petition";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(101, 13);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(470, 20);
+            this.txtURL.TabIndex = 2;
+            this.txtURL.Text = "https://petition.parliament.uk/petitions/635276";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Stats:";
+            // 
+            // Petition
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGetData);
+            this.Name = "Petition";
+            this.Text = "Petition Analytics";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Petition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PetitionGrapher
+{
+    public partial class Petition : Form
+    {
+        public Petition()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetData_Click(object sender, EventArgs e)
+        {
+            // validate the URL
+            // Get URL for the JSON data
+            // fetch the JSON data
+            // parse the JSON data
+            // visualise the JSON data
+        }
+    }
+}
